709ec7d9abd5f5b7245f4f062c6bdcb0c1d6f7ad
8nathan30 <114047357+8nathan30@users.noreply.github.com>
2022-11-03 18:40:13 +0800

    Add files via upload

---
+++ grade.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    
+    double m, p, f, pp, cp1, average;
+    
+    cout << "\n Enter your GRADES";
+    
+    cout << "\n MATH: ";
+    cin >> m;
+    
+    cout << "\n PHYSICS: ";
+    cin >> p;
+    
+    cout << "\n FILIPINO: ";
+    cin >> f;
+    
+    cout << "\n PURPOSIVE COMMUNICATION: ";
+    cin >> pp;
+    
+    cout << "\n COMPUTER PROGRAMMING 1: ";
+    cin >> cp1;
+    
+    average = (m + p + f + pp + cp1) / 5;
+    
+    cout << "Your Average is: " << average << endl;
+    cout << "Remarks: ";
+    
+    if (average >= 75) {
+	cout << "You Passed";
+    } else {
+	cout << "You Failed";
+    }
+    
+    return 0;
+}
