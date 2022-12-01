0deaefb17431064e779912c41b1d929552a93877
8nathan30 <114047357+8nathan30@users.noreply.github.com>
2022-12-01 17:04:32 +0800

    Add files via upload

---
+++ C++ Array.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int sum=0, math[6]={7, 5, 6, 12, 35, 27};
+	float average;
+	cout<<"The numbers are: ";
+
+	for(int i=0; i<6; i++) {
+
+		cout<<math[i]<<" ";
+
+	}
+ 
+	for(int i=0; i<6; i++) {
+
+        	sum+=math[i];
+
+	} 
+	cout<<"\nTheir Sum = "<<sum;
+
+    
+    
+	average=(float)sum/6;
+
+        cout<<"\nTheir Average = "<<average;
+    
+    return 0;
+
+}
